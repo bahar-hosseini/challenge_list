041de81228dfe477304b957a335711aa2aaf2144
Bahar Hosseini <bahar.hosseini@gmail.com>
2022-11-09 00:03:35 -0500

    add some sort algorithms

---
+++ sort_ algorithm.js
@@ -0,0 +1,47 @@
+// Bubble algorithm :
+const sampleArray = [21, 55, 32, 7, 4, 75, 28, 99, 18, 36, 2];
+
+const bubbleSort = (array) => {
+  const length = array.length;
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length; j++) {
+      if (array[j] > array[j + 1]) {
+        const temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+  return array;
+};
+
+//selection algorithm :
+//? itterate through array find min item
+
+const selection = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    let min = i;
+    let temp = array[i];
+    for (let j = 0; j < array.length; j++) {
+      if (array[i] < array[min]) {
+        min = j;
+      }
+    }
+    array[i] = array[min];
+    array[min] = temp;
+  }
+};
+
+//insertion algorithm:
+const insertion = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < array[0]) {
+      array.unshift(array.splice(i, 1)[0]);
+    } else {
+      for (let j = 1; j < i; j++) {
+        if (array[i] > array[j - 1] && array[i] < array[j])
+          array.unshift(j, 0, array.splice(i, 1)[0]);
+      }
+    }
+  }
+};
