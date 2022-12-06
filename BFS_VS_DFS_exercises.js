018fed593a664cdf368853141d0263bbe1b151df
Bahar Hosseini <bahar.hosseini@gmail.com>
2022-12-06 16:12:52 -0500

    add some BFS and DFS  questions

---
+++ BFS_VS_DFS_exercises.js
@@ -0,0 +1,17 @@
+//?If you know a solution is not far from the root of the tree:
+//BFS -> Starts searching the closest node to the parent;
+
+//?If the tree is very deep and solutions are rare:
+//BFS -> beacuse the solutions are rare we have to go over and over each br
+
+//?If the tree is very wide:
+//DFS -> memory wise if the speed in not important
+
+//?If solutions are frequent but located deep in the tree:
+//DFS -> memory wise if the speed in not important
+
+//?Determining whether a path exists between two nodes:
+//DFS
+
+//?Finding the shortest path:
+//BFS
