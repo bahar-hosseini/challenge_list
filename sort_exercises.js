a57c6c7abe26957b9b4bbf5545d79045f7d9fba7
Bahar Hosseini <bahar.hosseini@gmail.com>
2022-12-06 14:04:01 -0500

    add some sort questions

---
+++ sort_exercises.js
@@ -0,0 +1,23 @@
+//? #1 - Sort 10 schools around your house by distance:
+//Insertion sort -> small input, splace complexity O(1);
+/*********************************************************************/
+//? #2 - eBay sorts listings by the current Bid amount:
+//Radix sort or Counting Sort -> we are sure Bid is Int;
+/*********************************************************************/
+//? #3 - Sport scores on ESPN
+//Quick sort/Merge sort -> We are not sure if it's Int or decimal;
+/*********************************************************************/
+//? #4 - Massive database (can't fit all into memory) needs to sort through past year's user data
+//merge sort -> Performance is higher due to massive data;
+/*********************************************************************/
+//? #5 - Almost sorted Udemy review data needs to update and add 2 new reviews
+//Insertion sort -> They are pre sorted. so, we're gonna have smaller amount of input;
+/*********************************************************************/
+//? #6 - Temperature Records for the past 50 years in Canada
+//Quick sort -> It's fast and Kind of predictable and I'm not sure if It's Int to use Radix or Counting sort O(n log n)
+/*********************************************************************/
+//? #7 - Large user name database needs to be sorted. Data is very random.
+// Merge sort => Not quick sort beacuse it's hard to find an optimom pivot(Worst Case); O(n log n)
+/*********************************************************************/
+//? #8 - You want to teach sorting for the first time
+//Bubble sort OR Selection sort -> not efficient just easy to understand;
