e77f08d28b44d6e879867845d2b209067d093acb
Bahar Hosseini <bahar.hosseini@gmail.com>
2023-01-25 16:59:45 -0500

    solve challenge 67-leetcode(first method)

--- addBinary.js
+++ addBinary.js
@@ -7,23 +7,22 @@
  */
 let addBinary = function (a, b) {
   //! ***** first method: ****** :
-  * the highest supported number in js is 53 bit */
+  //* the highest supported number in js is 53 bit */
 
   if (a.length < 53 && b.length < 53) {
-
-    return ((parseInt(a,2) + parseInt(b,2)).toString(2));
+    return (parseInt(a, 2) + parseInt(b, 2)).toString(2);
   }
-  return ((BigInt("0b" + a) + BigInt("0b" + b)).toString(2));
+  return (BigInt('0b' + a) + BigInt('0b' + b)).toString(2);
 
   //! ***** Second method: *****
 
-  let sum = '';
+  // let sum = '';
 
-  // We want to start from right
-  let i = a.length - 1;
-  let j = b.length - 1;
+  // // We want to start from right
+  // let i = a.length - 1;
+  // let j = b.length - 1;
 
-  let carry = 0;
+  // let carry = 0;
 };
 
 //?codewars -> Kata : if a & b are Decimal numbers :
