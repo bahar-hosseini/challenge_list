0e8e863b40551d470ed6de6ee99ba8caf2b649bf
Bahar Hosseini <bahar.hosseini@gmail.com>
2023-01-25 02:03:56 -0500

    Add challenge 35 leetcode

---
+++ searchInsert.js
@@ -0,0 +1,29 @@
+//35. Search Insert Position
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+
+const searchInsert = function (nums, target) {
+  let left = 0;
+
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return left;
+};
+
+console.log(searchInsert([1, 2, 3, 4, 5, 6, 7, 8, 9], 3));
+
+console.log(searchInsert([], 3));
