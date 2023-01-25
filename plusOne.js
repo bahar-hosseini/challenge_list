aa573a368c24d109f04faf951edbd76678bce4cf
Bahar Hosseini <bahar.hosseini@gmail.com>
2023-01-25 09:57:28 -0500

    consider more cases to solve challenge 66 leetcode

--- plusOne.js
+++ plusOne.js
@@ -5,12 +5,16 @@
  * @return {number[]}
  */
 let plusOne = function (digits) {
-  if (digits[digits.length - 1] < 9) {
-    digits[digits.length - 1] += 1;
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] < 9) {
+      console.log(digits[i]);
+      digits[i] += 1;
+      return digits;
+    }
+    digits[i] = 0;
   }
-  if (digits[digits.length - 1] === 9) {
-    digits[digits.length - 1] = 1;
-    digits.push(0);
-  }
+  digits.unshift(1);
   return digits;
 };
+console.log(plusOne([9, 9]));
+console.log(plusOne([7]));
