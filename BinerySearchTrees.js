3f3c886dd5bad7dbd49cba72cb35bb7c82bb58ad
Bahar Hosseini <bahar.hosseini@gmail.com>
2023-01-27 11:40:29 -0500

    implement a binary tree - lookup method

--- BinerySearchTrees.js
+++ BinerySearchTrees.js
@@ -1,8 +1,8 @@
 //Implementing a Binary Tree
 class Node {
   constructor(value) {
-    this.left = left;
-    this.right = right;
+    this.left = null;
+    this.right = null;
     this.value = value;
   }
 }
@@ -37,7 +37,22 @@ class BinerySearchTree {
       }
     }
   }
-  lookup(value) {}
+  lookup(value) {
+    if (!this.root) {
+      return false;
+    }
+    let currentNode = this.root;
+    while (currentNode) {
+      if (value < currentNode) {
+        currentNode = currentNode.left;
+      } else if (value > currentNode) {
+        currentNode = currentNode.right;
+      } else if ((currentNode.value = value)) {
+        return currentNode;
+      }
+    }
+    return false;
+  }
 }
 
 const tree = new BinerySearchTree();
@@ -47,3 +62,6 @@ tree.insert(6);
 tree.insert(20);
 tree.insert(170);
 tree.insert(50);
+
+tree.lookup(4);
+tree.lookup(9);
