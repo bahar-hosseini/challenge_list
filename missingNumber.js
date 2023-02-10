f1ddc5b1670031fac5aa1ccfd2fa7355df7f3869
Bahar Hosseini <bahar.hosseini@gmail.com>
2023-02-10 00:57:11 -0500

    add more methods to solve challenge 268 leetcode

--- missingNumber.js
+++ missingNumber.js
@@ -1,14 +1,34 @@
 //268. Missing Number
 
 let missingNumber = function (nums) {
-  //first method:
+  //!first method:
   const gSum = (nums.length * (nums.length + 1)) / 2;
   const nSum = nums.reduce((acc, curr) => acc + curr);
   return gSum - nSum;
+
+  //!second method:
+  // let i = 0;
+  // while (i < nums.length) {
+  //   if ((nums.includes(i))) {
+  //     i++;
+  //   } else {
+  //     return i;
+  //   }
+
+  // }
+
+  //! third method:
+  // nums = nums.sort();
+  // for (let i = 0; i < nums.length; i++) {
+  //   if (nums[i] !== i) {
+  //     return i;
+  //   }
+  // }
 };
 
 console.log(missingNumber([3, 0, 1]));
 
 //input : array
 //output: number
+//* first number of range is 0
 //All the numbers of nums are unique.
