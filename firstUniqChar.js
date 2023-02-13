1629b1744eb0cdaae0f7d46d83a5aa21dd76199f
Bahar Hosseini <bahar.hosseini@gmail.com>
2023-02-13 11:39:46 -0500

    solve challenge 387 leetcode

--- firstUniqChar.js
+++ firstUniqChar.js
@@ -1,1 +1,21 @@
 // 387. First Unique Character in a String
+
+let firstUniqChar = function(s) {
+  let obj = {};
+  for (let i = 0; i < s.length; i++) {
+    obj[s[i]] = obj[s[i]] + 1 || 1;
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    if (obj[s[i]] === 1) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+console.log(firstUniqChar('leeetcode'));
+
+//Q find first non-repeating character
+//input : string
+//output: index number
