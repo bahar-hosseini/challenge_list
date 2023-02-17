c27094928c3d44f2d175a84e725f4b84d2b7a5aa
Bahar Hosseini <bahar.hosseini@gmail.com>
2023-02-16 23:23:33 -0500

    challenge 160 leetcode

--- hasCycle.js
+++ hasCycle.js
@@ -6,9 +6,10 @@ let hasCycle = function(head) {
   while (fast && fast.next) {
     fast = fast.next.next;
     slow = slow.next;
+    if (fast === slow) return true;
   }
-  return  fast === slow;
+  return false;
 };
 
-console.log(hasCycle[3,2,0,-4]);
+
 
