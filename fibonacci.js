0eef38c9d99aaa25aeebe5cc291ad860a98f2595
Bahar Hosseini <bahar.hosseini@gmail.com>
2023-03-03 18:37:02 -0500

    fibonacci return in array

--- fibonacci.js
+++ fibonacci.js
@@ -24,3 +24,18 @@ const fibonacciClosure = (n) => {
     }
   };
 };
+
+//! fibonacci ---> Return Array
+//Write a function that takes in N and outputs an array with the result of the fibs from 0 to N
+// input : N int
+// outPut : Array []: of Fib 0 to N
+// 0 , 1 , 1, 2, 3 , 5 ,8
+
+const fib = (n)=>{
+  let result = [0,1];
+  for (let i = 2; i < n; i++) {
+    result.push(result[i - 1] + result[i - 2]);
+  }
+  return result.slice(0, n + 1);
+};
+console.log(fib(2));
