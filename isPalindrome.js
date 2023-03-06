6ad8f3b1def93321e0d36f387bd91b765d189c9f
Bahar Hosseini <bahar.hosseini@gmail.com>
2023-03-05 23:17:36 -0500

    add other methods to solve 125 challenge

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,9 +1,22 @@
 //125. Valid Palindrome
 
-let isPalindrome = function (s) {
+let isPalindrome = function(s) {
   s = s.toLowerCase().replace(/[\W_]/g, '');
   let reversed = s.split('').reverse().join('');
   return s === reversed;
+};
+
+//Method 2
+let isPalindrome2 = function(s) {
+
+  s = s.replace(/[^0-9a-z]/ig,"").toLowerCase();
+  let midLength = Math.floor(s.length / 2);
+  for (let i = 0; i < midLength; i++) {
+    if (s[i] !== s[s.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 console.log(isPalindrome('amanaplanacanalpanama'));
