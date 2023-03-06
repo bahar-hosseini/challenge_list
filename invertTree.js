d6b005598045863d5db0a38d401c367031e52fb5
Bahar Hosseini <bahar.hosseini@gmail.com>
2023-03-06 10:35:04 -0500

    challenge  226 leetcode

---
+++ invertTree.js
@@ -0,0 +1,20 @@
+// 226. Invert Binary Tree
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+let invertTree = function(root) {
+  if (root) {
+    [root.left,root.right] = [invertTree(root.right),invertTree(root.left)];
+  }
+  return root;
+};
