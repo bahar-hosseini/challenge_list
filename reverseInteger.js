82f7cee52ccf2e1caa673661481cd84de243d4ba
Bahar Hosseini <bahar.hosseini@gmail.com>
2023-05-18 20:15:17 -0400

    Add challenge 7 of leetcode

---
+++ reverseInteger.js
@@ -0,0 +1,8 @@
+//  7. Reverse Integer;
+
+const reverse = (x) =>{
+  if (x < 0) return reverse(-x) * -1;
+  let result = x.toString().split('').reverse().join('');
+
+  return result < 2 ** 31 -1 ? result : 0;
+};
