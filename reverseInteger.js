0a002f5e79e13002d1da7c5e4587af543c0e4f5e
Bahar Hosseini <bahar.hosseini@gmail.com>
2023-05-18 20:21:00 -0400

    fix eslint warning

--- reverseInteger.js
+++ reverseInteger.js
@@ -4,5 +4,6 @@ const reverse = (x) =>{
   if (x < 0) return reverse(-x) * -1;
   let result = x.toString().split('').reverse().join('');
 
-  return result < 2 ** 31 -1 ? result : 0;
+  return result < 2 ** 31 - 1 ? result : 0;
 };
+console.log(reverse(-123));
