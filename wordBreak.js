4609c93bac534d19a9bf82930d355452ae2705d1
Bahar Hosseini <bahar.hosseini@gmail.com>
2023-05-23 23:24:00 -0400

    Add challenge 139 wordBreak leetcode

---
+++ wordBreak.js
@@ -0,0 +1,21 @@
+// 139. Word Break
+
+const wordBreak = (s, wordDict) =>{
+  const table = new Array(s.length + 1).fill(false);
+  table[0] = true;
+  console.log(table);
+  for (let i = 0; i < table.length; i ++) {
+    if (table[i] === false) continue;
+    for (let j = i + 1; j < table.length; j++) {
+
+      if (wordDict.includes(s.slice(i,j))) {
+        table[j] = true;
+      }
+    }
+  }
+  console.log(table);
+  return (table[table.length - 1]);
+};
+
+console.log(wordBreak("catsandog",["cats","dog","sand","and","cat","og"]));
+// console.log(wordBreak("leetcode",["leet","code"]));
